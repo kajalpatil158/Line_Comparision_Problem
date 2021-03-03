af719626e47bdd277e8c655f5da3b8e12d51bbbf
Kajal <kajalpatil158@gmail.com>
2021-03-03 19:52:50 +0530

    [Kajal]ADD: Calculate_Length

--- Line_Comparision_Problem/Line_Comparision_Problem/Line_Comparision_Problem/Program.cs
+++ Line_Comparision_Problem/Line_Comparision_Problem/Line_Comparision_Problem/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace Line_Comparision_Problem
+namespace Calculate_Length
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome To Line Comparision Computation!!!");
+            double Length_Of_Line;
+            Console.WriteLine("Enter X1 ");
+            int X1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X2 ");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y1 ");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y2 ");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+            Length_Of_Line = (Math.Sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2));
+            //Console.WriteLine(Math.Sqrt(Length_Of_Line));
+            Console.WriteLine("Lenth of line is {0}", Length_Of_Line);
         }
     }
 }
