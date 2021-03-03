f110a1c2d9093ef8dea1722571a4f227b0214434
Kajal <kajalpatil158@gmail.com>
2021-03-03 19:47:54 +0530

    [Kajal]ADD: Welcome_Msg

---
+++ Line_Comparision_Problem/Line_Comparision_Problem/Line_Comparision_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Comparision_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome To Line Comparision Computation!!!");
+        }
+    }
+}
